4e8671409c151d2eaefb0dfff6c2f2301d542991
dennishurst <dennis.hurst@saltworkssecurity.com>
2015-03-23 11:28:32 -0400

    Service Master checking

--- Utilities/PlayImporter.js
+++ Utilities/PlayImporter.js
@@ -4,17 +4,13 @@ fs = require('fs');
 
 
 
-var sScene = {
+var oScene = {
     "_id": "1",
     "dataType": "Scene",
     "description": "Act 2, Scene 2: Capulet's orchard.",
     "act": 1,
     "lines": [
-      {
-          "role": "",
-          "stageDirection": "Enter ROMEO",
-          "line": ""
-      }
+      
     ]
 };
 
@@ -22,34 +18,55 @@ var sScene = {
 
 var aLines = fs.readFileSync('./SamplePlay.txt').toString().split("\n");
 
-sScene.description = aLines[0];
+oScene.description = aLines[0];
 
 var sRole = "";
 var bNextIsRole = false;
+var oCurrentLine = {
+    "role": "",
+    "stageDirection": "",
+    "line": ""
+}
+
 for (var i = 0; i < aLines.length; i++) {
     
     if (bNextIsRole && aLines[0].trim().length > 0){
-        sRole = aLines[0];
+        //Create a new line
+        oCurrentLine = {
+            "role": aLines[i],
+            "stageDirection": "",
+            "line": ""
+        }        
+        
+        bNextIsRole = false;
     }
-    else if (aLines[0].trim().length === 0){
+    else if (aLines[i].trim().length === 0){
         bNextIsRole = true;
+
+        if (i != 0) {
+            oScene.lines.push(oCurrentLine);
+        }
+
+    }
+    else if (aLines[i].substring(0,1) === '[')
+    {
+        //console.log('Direction found:' + aLines[i])
+        oCurrentLine.stageDirection = aLines[i];
+    }
+    else {
+        oCurrentLine.line += aLines[i] + "<br />";
     }
 }
 
 
+console.log("Des:" + oScene.description);
 
-ROMEO
-[Enter ROMEO] 
+for (var i = 0; i < oScene.lines.length; i++) {
 
-ROMEO
-He jests at scars that never felt a wound.
-
-ROMEO
-[JULIET appears above at a window]
-But, soft! what light through yonder window breaks?
-It is the east, and Juliet is the sun.
-
-
+    console.log('role: ' + oScene.lines[i].role + "\n");
+    console.log('stateDirection:' + oScene.lines[i].stageDirection)
+    console.log('line:' + oScene.lines[i].line);
+}
 
 
 
