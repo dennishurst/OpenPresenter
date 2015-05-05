430349ca4513f30b90d7ed93dff0b00b12038f56
dennishurst <dennis.hurst@saltworkssecurity.com>
2015-05-04 23:49:01 -0400

    Final Exam
    There are 5 errors,
    2 in OpenPresenter.js
    1 related to serviceWebMaster.html
    3 in index.html or related to it.

--- web/OpenPresenter.js
+++ web/OpenPresenter.js
@@ -13,34 +13,26 @@ var sTSettings = fs.readFileSync("settings.json");
 var oSettings = JSON.parse(sTSettings);
 console.log("currentService:" + oSettings.currentService_id);
 
+console.log(Starting program, now let's load a handler.);
 
-
-
-app.get('/songs/*', function (req, res) {
+app.get('/*', function (req, res) {
     var sFile = req.originalUrl;
-    sFile = sFile.substring(7, sFile.length);
-    
-    if (sFile.length === 0) {
-        sFile = util.fxGetSongs();
-        app.set('json_spaces', 4);
-        res.json(sFile);
-        return;
-    }
-    
+    sFile = sFile.substring(2, sFile.length);
 
+    if (sFile.length === 0)
+    {
+        sFile = "index.html";
+    }
+    console.log(sFile);
+    
     res.sendFile(sFile,
         {
             maxAge: 1,    // 24* 60* 60* 1000, 
-            root: './root/songs/',
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8'
-            }
-	
+            root: './root/'
         },
         function (err) {
             if (err) {
                 console.log("Error serving:" + sFile);
-                console.log(err);
                 res.status(err.status).end();
             }
             else {
@@ -50,12 +42,11 @@ app.get('/songs/*', function (req, res) {
 });
 
 
-
 app.get('/api/*', function (req, res) {
     var myObj = {
         FirstName: "Dennis",
         LastName: "Hurst",
-        "Courses":  [{"Class": "CS", "Grade":"A"}, {"Class": "English", "Grade": "C"}]
+        "Courses": [{ "Class": "CS", "Grade": "A" }, { "Class": "English", "Grade": "C" }]
 
     }
 
@@ -74,7 +65,7 @@ app.get('/user/:userid', function (req, res) {
     console.log(" Path:\t " + req.path);
     console.log(" Host:\t " + req.host);
     console.log(" Method:\t " + req.method);
-    console.log(" Query:\t " + JSON.stringify( req.query));
+    console.log(" Query:\t " + JSON.stringify(req.query));
     console.log(" Fresh:\t " + req.fresh);
     console.log(" Stale:\t " + req.stale);
     console.log(" Secure:\t " + req.secure);
@@ -87,25 +78,31 @@ app.get('/user/:userid', function (req, res) {
     res.send(sT);
 });
 
-
-app.get('/*', function (req, res) {
+app.get('/songs/*', function (req, res) {
     var sFile = req.originalUrl;
-    sFile = sFile.substring(1, sFile.length);
+    sFile = sFile.substring(7, sFile.length);
 
-    if (sFile.length === 0)
-    {
-        sFile = "index.html";
+    if (sFile.length === 0) {
+        sFile = util.fxGetSongs();
+        app.set('json_spaces', 4);
+        res.json(sFile);
+        return;
     }
-    console.log(sFile);
-    
+
+
     res.sendFile(sFile,
         {
             maxAge: 1,    // 24* 60* 60* 1000, 
-            root: './root/'
+            root: './root/songs/',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+
         },
         function (err) {
             if (err) {
                 console.log("Error serving:" + sFile);
+                console.log(err);
                 res.status(err.status).end();
             }
             else {
